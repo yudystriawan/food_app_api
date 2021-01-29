ba60e69451077f5a9ad8c53833bd0e6374e0888a
baligatra <mac@baligatra.com>
2021-01-29 15:27:22 +0800

    add sentry

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/debug-sentry', function () {
+    throw new Exception('My first Sentry error!');
+});
