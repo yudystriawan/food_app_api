c62bb17e75a5b1cfbe5a58c062b2ebb03c67e3e4
yudystriawan <yudistriawan@gmail.com>
2021-01-31 05:45:25 +0800

    init controller

---
+++ app/Http/Controllers/Api/RestoController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Resto;
+use Illuminate\Http\Request;
+
+class RestoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Resto  $resto
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Resto $resto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Resto  $resto
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Resto $resto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Resto  $resto
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Resto $resto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Resto  $resto
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Resto $resto)
+    {
+        //
+    }
+}
