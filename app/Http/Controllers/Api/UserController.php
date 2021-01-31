dda1b49341d4ca99ecc3bd73bd06343ea89c7333
yudystriawan <yudistriawan@gmail.com>
2021-01-31 21:40:54 +0800

    implement userController method

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Actions\Fortify\PasswordValidationRules;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    use PasswordValidationRules;
     /**
      * Display a listing of the resource.
      *
@@ -15,17 +17,9 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
-    }
+        $users = User::all();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return response()->json(['data' => $users], 200);
     }
 
     /**
@@ -36,7 +30,20 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|string|unique:users',
+            'password' => $this->passwordRules(),
+        ]);
+
+        $data = $request->all();
+        $data['password'] = bcrypt($request->password);
+        $data['verified'] = User::UNVERIFIED_USER;
+        $data['admin'] = User::REGULAR_USER;
+
+        $user = User::create($data);
+
+        return response()->json(['data' => $user], 201);
     }
 
     /**
@@ -47,18 +54,7 @@ class UserController extends Controller
      */
     public function show(User $user)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(User $user)
-    {
-        //
+        return response()->json(['data' => $user], 200);
     }
 
     /**
@@ -70,7 +66,47 @@ class UserController extends Controller
      */
     public function update(Request $request, User $user)
     {
-        //
+        // $request->validate([
+        //     'email' => 'email|unique:users,email,' . $user->id,
+        //     'password' => $this->passwordRules(),
+        // ]);
+
+        if ($request->has('name')) {
+            $user->name = $request->name;
+        }
+
+        if ($request->has('email') && $user->email != $request->email) {
+            $user->verified = User::UNVERIFIED_USER;
+            $user->email = $request->email;
+        }
+
+        if ($request->has('password')) {
+            $user->password = bcrypt($request->password);
+        }
+
+        if ($request->has('address')) {
+            $user->address = $request->address;
+        }
+
+        if ($request->has('house_number')) {
+            $user->houseNumber = $request->houseNumber;
+        }
+
+        if ($request->has('phone_number')) {
+            $user->phoneNumber = $request->phoneNumber;
+        }
+
+        if ($request->has('city')) {
+            $user->city = $request->city;
+        }
+
+        if (!$user->isDirty()) {
+            return response()->json(['error' => 'You need to specify a different value to update'], 420);
+        }
+
+        $user->save();
+
+        return response()->json(['data' => $user], 200);
     }
 
     /**
@@ -81,6 +117,8 @@ class UserController extends Controller
      */
     public function destroy(User $user)
     {
-        //
+        $user->delete();
+
+        return response()->json(['data' => $user], 200);
     }
 }
