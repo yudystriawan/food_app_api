76beee34b8efeaa86b3e527cd00013af79d3ba29
yudystriawan <yudistriawan@gmail.com>
2021-02-02 22:11:39 +0800

    add operation foodCustomerTransaction

--- app/Http/Controllers/Api/Food/FoodCustomerTransactionController.php
+++ app/Http/Controllers/Api/Food/FoodCustomerTransactionController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers\Api\Food;
 
+use App\Http\Controllers\ApiController;
 use App\Http\Controllers\Controller;
 use App\Models\Customer;
 use App\Models\Food;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
-class FoodCustomerTransactionController extends Controller
+class FoodCustomerTransactionController extends ApiController
 {
 
     /**
@@ -25,9 +28,23 @@ class FoodCustomerTransactionController extends Controller
         if (!$customer->isVerified()) {
             return $this->errorResponse('The customer must be a verified user', 409);
         }
-        
+
         if (!$food->resto->isVerified()) {
             return $this->errorResponse('The resto must be a verified user', 409);
         }
+
+        // dd($food->price);
+
+        return DB::transaction(function () use ($request, $food, $customer) {
+            $transaction = Transaction::create([
+                'quantity' => $request->quantity,
+                'total' => $request->quantity * $food->price,
+                'status' => Transaction::PENDING,
+                'customer_id' => $customer->id,
+                'food_id' => $food->id,
+            ]);
+
+            return $this->showOne($transaction);
+        });
     }
 }
--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -25,14 +25,14 @@ class TransactionFactory extends Factory
     public function definition()
     {
 
-        $resto = Resto::has('food')->get()->random();
+        $resto = Resto::has('foods')->get()->random();
         $customer = User::all()->except($resto->id)->random();
 
         return [
             'quantity' => $this->faker->numberBetween(1,3),
             'total' => $this->faker->randomDigit,
             'status' => $this->faker->randomElement([Transaction::PENDING,Transaction::CANCELED,Transaction::ON_DELIVERY,Transaction::DELIVERED]),
-            'food_id' => $resto->food->random()->id,
+            'food_id' => $resto->foods->random()->id,
             'customer_id' => $customer->id,
         ];
     }
