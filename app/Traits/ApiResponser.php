d38c80cadfb9aaf3c9423fee22fbc8ee5f270a3a
baligatra <mac@baligatra.com>
2021-02-04 10:16:50 +0800

    add pagination

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -2,8 +2,12 @@
 
 namespace App\Traits;
 
+use GrahamCampbell\ResultType\Result;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Validator as ValidationValidator;
 
 trait ApiResponser
 {
@@ -28,6 +32,7 @@ trait ApiResponser
 
         $collection = $this->filterData($collection, $tranformer);
         $collection = $this->sortData($collection, $tranformer);
+        $collection = $this->paginate($collection);
         $collection = $this->transformData($collection, $tranformer);
 
         return $this->successResponse($collection, $code);
@@ -50,12 +55,11 @@ trait ApiResponser
     protected function filterData(Collection $collection, $transformer)
     {
         foreach (request()->query() as $query => $value) {
-            if ($query != 'sort_by') {
+            if ($query != 'sort_by' && $query != 'page') {
                 $attribute = $transformer::originalAttribute($query);
-                
+
                 $collection = $collection->where($attribute, $value);
             }
-
         }
 
         return $collection;
@@ -72,11 +76,26 @@ trait ApiResponser
         return $collection;
     }
 
+    protected function paginate(Collection $collection)
+    {
+        $page = LengthAwarePaginator::resolveCurrentPage();
+        $perPage = 15;
+
+        $results = $collection->slice(($page - 1) * $perPage, $perPage)->values();
+
+        $paginated = new LengthAwarePaginator($results, $collection->count(), $perPage, $page, [
+            'path' => LengthAwarePaginator::resolveCurrentPath(),
+        ]);
+
+        $paginated->appends(request()->all());
+
+        return $paginated;
+    }
+
     protected function transformData($data, $transformer)
     {
         $newCollection = fractal($data, new $transformer)->toArray();
 
         return $newCollection;
-    
     }
 }
